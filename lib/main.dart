9914564539a58280abd899598331c36cfcc8db22
Nathan <48697917+RinalduzziNathan@users.noreply.github.com>
2022-03-11 10:47:03 +0100

    Reusing the spotlist adapted to launchList

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_project/manager/launch_manager.dart';
 import 'package:flutter_project/model/launch.dart';
+import 'package:flutter_project/view/launchList.dart';
 import 'package:flutter_project/view_model/next_launch_vm.dart';
 import 'package:flutter_project/view_model/upcoming_launches.dart';
 import 'package:provider/provider.dart';
@@ -43,13 +44,9 @@ class MyHomePage extends StatelessWidget {
               ? const Center(
             child: CircularProgressIndicator(),
           )
-              : ListView.builder(
-            itemBuilder: (context, position) => Padding(
-              padding: const EdgeInsets.fromLTRB(20, 10, 20, 20),
-              child: Text(upcomingLaunches.launches?[position].name ?? ''),
-            ),
-            itemCount: upcomingLaunches.launches?.length,
-          ),
+
+                  : LaunchList(launches: upcomingLaunches.launches!)
+
         ),
       ),
     );
---
+++ lib/view/launchList.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_project/model/launch.dart';
+
+import 'image_placeholder.dart';
+
+
+class LaunchList extends StatelessWidget {
+  final List<Launch> launches;
+  final Function(Launch, bool)? onFavoriteChanged;
+
+  const LaunchList({Key? key, required this.launches, this.onFavoriteChanged})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return launches.isNotEmpty ? ListView.builder(
+      itemBuilder: (context, position) {
+        Launch launch = launches[position];
+        return InkWell(
+          onTap: () async {
+           // bool oldFavorite = SpotManager().isSpotFavorite(spot.id);
+
+            //var newFavorite = await Navigator.of(context).pushNamed(SpotDetail.route,
+              //  arguments: SpotDetailArguments(spot: spot));
+
+           // if (newFavorite is bool && newFavorite != oldFavorite) {
+             // onFavoriteChanged?.call(spot, false);
+            //}
+          },
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              SizedBox(
+                  width: 100,
+                  height: 100,
+                  child: Image.network(
+                    launch.links?.patch?.small ?? '',
+                    fit: BoxFit.cover,
+                    errorBuilder: (context, child, stack) {
+                      return const ImagePlaceholder();
+                    },
+                  )),
+              const SizedBox(
+                width: 16,
+              ),
+              Expanded(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      launch.name ?? '',
+                      style: const TextStyle(fontSize: 16),
+                    ),
+                    const SizedBox(
+                      height: 8,
+                    ),
+                    Text("Catégorie : ${launch.id ?? 'Inconnue'}")
+                  ],
+                ),
+              ),
+
+              const SizedBox(width: 16,)
+            ],
+          ),
+        );
+      },
+      itemCount: launches.length,
+    ) : const Center(child: Text('Aucun spot'),);
+  }
+}
