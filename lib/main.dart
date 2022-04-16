57a156cd8f25f89ebd70575f6bad6058b446b0f3
Nathan <n.rinalduzzi@gmail.com>
2022-04-16 20:46:03 +0200

    Revert "Merge branch 'feature/notification'"
    This reverts commit bd7362fbf0c54785912d1bbb8c63e5a25768390a, reversing
    changes made to 8f0813872cc4baef6c953972699033059006afe9.

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,9 @@ import 'package:flutter/material.dart';
 
 import 'app/app.dart';
 import 'manager/database_manager.dart';
-import 'manager/notification_manager.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await NotificationService().init(); //
   await DatabaseManager().init();
   runApp(const App());
 }
